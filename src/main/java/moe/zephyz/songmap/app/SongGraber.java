444fa1fa289a661a3972605c8d2aec07af78f7bf
zhaar <zephyz.zhaar@gmail.com>
2014-05-11 04:18:46 +0200

    added base classe songGraber and base methods for main calss

---
+++ src/main/java/moe/zephyz/songmap/app/SongGraber.java
@@ -0,0 +1,15 @@
+package moe.zephyz.songmap.app;
+
+/**
+ * Created by zephyz on 11/05/14.
+ */
+public class SongGraber {
+    public SongGraber(){
+
+    }
+
+    public String getCurrentSong(){
+        return "";
+    }
+
+}
